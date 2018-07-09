e0caf0adbc3875df8dccfd3162b81cdcc5427351
Chuck Danger <beneth@gmail.com>
2018-07-09 14:31:50 -0500

    blah
    blah

--- TailB3Log.ps1
+++ TailB3Log.ps1
@@ -1,1 +1,3 @@
-﻿Get-Content $env:AppData\B3-CoinV2\debug.log –Wait
+﻿filter timestamp {"$(Get-Date -Format G): $_"}
+
+Get-Content $env:AppData\B3-CoinV2\debug.log –Wait | Where-Object {$_ -match "blocks"} | timestamp
